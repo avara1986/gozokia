58597d4caf29d328457daabbd4bde1eb95efbf5a
avara1986 <a.vara.1986@gmail.com>
2015-12-20 21:04:04 +0100

    Created a example file settings

---
+++ settings.example.py
@@ -0,0 +1,17 @@
+GOOGLE_TRANSLATE_KEY = ""
+
+GOZOKIA_INPUT_TYPE = "terminal_txt"
+GOZOKIA_OUTPUT_TYPE = "terminal_txt"
+GOZOKIA_AUDIO_PLAYER = "mpg123"
+GOZOKIA_LANGUAGE = "en-US"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mongodb',
+        'NAME': 'gozokia',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '92.168.100.7',
+        'PORT': '27019',
+    }
+}
