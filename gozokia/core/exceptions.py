02a9d7e5828dc9a3d8f885aa8d1776c19e4d4fbe
avara1986 <a.vara.1986@gmail.com>
2015-12-20 21:03:23 +0100

    Created exceptions file

---
+++ gozokia/core/exceptions.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+"""
+Core exceptions
+"""
+
+
+class GozokiaError(Exception):
+    '''raise this when there's a lookup error for my app'''
+    pass
+
+
+class ImproperlyConfigured(Exception):
+    """Django is somehow improperly configured"""
+    pass
