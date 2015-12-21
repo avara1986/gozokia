2650526ab9fd056c14ebb3ac3acef66e0a34debb
avara1986 <a.vara.1986@gmail.com>
2015-12-21 23:05:53 +0100

    Added no input audio excepion

--- gozokia/i_o/io_voice.py
+++ gozokia/i_o/io_voice.py
@@ -26,25 +26,28 @@ class VoiceRecognizerMixin(object):
 
     def listen_audio(self, language):
         # use the default microphone as the audio source
-        with m as source:
-            r.adjust_for_ambient_noise(source)
-            print("Set minimum energy threshold to {}".format(r.energy_threshold))
-            # listen for the first phrase and extract it into audio data
-            input_audio = r.listen(source)
-            print("Got it! Now to recognize it...")
-            try:
-                # using Google Speech Recognition
-                input_result = r.recognize_google(input_audio, language=language)
-                if str is bytes:  # this version of Python uses bytes for strings (Python 2)
-                    input_result = input_result.encode('utf8')
-            except sr.UnknownValueError:
-                input_result = ("Google Speech Recognition could not understand audio")
-            except sr.RequestError as e:
-                input_result = ("Could not request results from Google Speech Recognition service; {0}".format(e))
-            # speech is unintelligible
-            except LookupError:
-                input_result = ("I don't understand you")
-        return input_result
+        try:
+            with m as source:
+                r.adjust_for_ambient_noise(source)
+                print("Set minimum energy threshold to {}".format(r.energy_threshold))
+                # listen for the first phrase and extract it into audio data
+                input_audio = r.listen(source)
+                print("Got it! Now to recognize it...")
+                try:
+                    # using Google Speech Recognition
+                    input_result = r.recognize_google(input_audio, language=language)
+                    if str is bytes:  # this version of Python uses bytes for strings (Python 2)
+                        input_result = input_result.encode('utf8')
+                except sr.UnknownValueError:
+                    input_result = ("Google Speech Recognition could not understand audio")
+                except sr.RequestError as e:
+                    input_result = ("Could not request results from Google Speech Recognition service; {0}".format(e))
+                # speech is unintelligible
+                except LookupError:
+                    input_result = ("I don't understand you")
+            return input_result
+        except Exception as e:
+            raise GozokiaIoError(e)
 
 
 class VoiceResponseMixin(object):
