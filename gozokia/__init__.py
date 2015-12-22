aa1337d4127e42a2190ee07d8156ab66c5411c06
avara1986 <a.vara.1986@gmail.com>
2015-12-22 20:25:23 +0100

    Added Gozokia class to init

--- gozokia/__init__.py
+++ gozokia/__init__.py
@@ -3,9 +3,9 @@
 Default settings of Gozokia.
 Its coulds be defined on your Django settings
 '''
+from .gozokia import Gozokia
+
 __author__ = "Alberto Vara"
 __email__ = "a.vara.1986@gmail.com"
 __version__ = "0.2"
 __license__ = "MIT"
-
-from .gozokia import Gozokia
