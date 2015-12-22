653f3adb20103ed080ef9640751b7f1c7b35055f
avara1986 <a.vara.1986@gmail.com>
2015-12-22 20:57:43 +0100

    Update example with import classes

--- example/main.py
+++ example/main.py
@@ -3,10 +3,9 @@
 import os
 import sys
 from pymongo import MongoClient
-
 sys.path.insert(0, os.getcwd())
 from gozokia import Gozokia
-
+from example.my_class import MyClass
 
 # First, declare our settings file:
 os.environ.setdefault("GOZOKIA_SETTINGS_MODULE", "settings")
@@ -16,18 +15,22 @@ goz = Gozokia()
 # client = MongoClient("mongodb://192.168.100.7:27019")
 
 
-# Register our rules
-@goz.rule('test_func')
+# Register our function rules
+# @goz.rule('test_func')
 def my_function():
     print('Hello function')
 
+# Register our class rules
+goz.rule('test_class')(MyClass)
 
-# Register our rules
-@goz.rule('test_class')
-class my_class():
+
+# Register our class rules
+@goz.rule('test_class2')
+class MyClass2():
     def my_method(self):
         print('Hello method')
 
+# Run Gozokia console
 if __name__ == '__main__':
     goz.initialize()
     print('\n*** Gozokia ***')
